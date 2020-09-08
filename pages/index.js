6283bf8951a6db7dfc8d9a91edb16c73ce251c73
Ashutosh Sharma <ashutoshsharma@AT-MAC2018-001.local>
2020-09-08 23:24:19 +0530

    minor seo optimizations

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,14 @@ export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>Create Next App</title>
+        <meta charset="utf-8" />
+        <meta http-equiv="content-type" content="text/html; charset=utf-8"></meta>
+        <title>Ashutosh Sharma | Frontend Architect at housing.com | Ex Times Internet, Goibibo, Zoho | Javascript, React, rust</title>
+        <meta name="description" content="Ashutosh Sharma is a frontend architect at housing.com and is proficient in high-performance web applications. He worked extensively on Javascript, React, CI/CD, performance tracking, and writing scalable frontend application" />
+        <meta name="keywords" content="Ashutosh Sharma, Frontend Architect, Frontend performance, React, Javascript" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://ashu.online/" />
       </Head>
       <Description />
       <Experiance />
