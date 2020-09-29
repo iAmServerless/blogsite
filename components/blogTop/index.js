c78726b80b06412acf0991c3da418481f0034a65
Ashutosh Sharma <ashutoshsharma@AT-MAC2018-001.local>
2020-09-29 16:29:08 +0530

    added logo

--- components/blogTop/index.js
+++ components/blogTop/index.js
@@ -1,8 +1,10 @@
 import styles from './blog.module.css';
+import Link from 'next/link'
 import utilStyles from './../../styles/utils.module.css';
 
 export default function Description(props) {
     return <div className={styles.container}>
+        <Link href='/'><a className={styles.logo}><img src="/logo.png" /></a></Link>
         <img className={styles.heroImage} src={props.imgSrc} alt={props.imageAlt} />
         <div className={`${styles.title}`}>
             <h1 className={utilStyles.gradient2}>Lighthouse Auditing, things you should know</h1>
