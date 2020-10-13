8f165d1153ddf92625a63addd86000c73b83bedc
Ashutosh Sharma <ashutoshsharma@AT-MAC2018-001.local>
2020-10-13 16:53:50 +0530

    changed blog title

--- components/blogTop/index.js
+++ components/blogTop/index.js
@@ -7,7 +7,7 @@ export default function Description(props) {
         <Link href='/'><a className={styles.logo}><img src="/logo.png" /></a></Link>
         <img className={styles.heroImage} src={props.imgSrc} alt={props.imageAlt} />
         <div className={`${styles.title}`}>
-            <h1 className={`${utilStyles.gradient2}`}>Do not chase the perfect lighthouse performance score</h1>
+            <h1 className={`${utilStyles.gradient2}`}>Lighthouse: Expectation vs. Reality</h1>
             <h4 className={`${utilStyles.gradient2}`}>Make an informative decision before picking another optimization suggestion</h4>
         </div>
     </div>
