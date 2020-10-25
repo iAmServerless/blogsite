047d082b9fac9d29ad7eb51e3e91533a5938ce01
Ashutosh Sharma <ashutoshsharma@AT-MAC2018-001.local>
2020-10-25 15:15:41 +0530

    CLS fix and sitemap

--- components/blogTop/index.js
+++ components/blogTop/index.js
@@ -5,7 +5,7 @@ import utilStyles from './../../styles/utils.module.css';
 export default function Description(props) {
     return <div className={styles.container}>
         <Link href='/'><a className={styles.logo}><img src="/logo.png" /></a></Link>
-        <img className={styles.heroImage} src={props.imgSrc} alt={props.imageAlt} />
+        <img className={styles.heroImage} src={props.imgSrc} width={props.width} height={props.height} alt={props.imageAlt} />
         <div className={`${styles.title}`}>
             <h1 className={`${styles.heading}`}>Lighthouse: Expectation vs. Reality</h1>
             <h4 className={`${styles.subHeading}`}>Make an informative decision before picking another optimization suggestion</h4>
